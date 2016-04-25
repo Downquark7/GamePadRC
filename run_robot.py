9c571922ed8daa3f52af6b9890b976a4c2c5a591
Downquark7 <mail2emn@gmail.com>
2016-04-24 20:23:18 -0500

    Update run_robot.py

--- run_robot.py
+++ run_robot.py
@@ -84,7 +84,7 @@ if __name__ == "__main__":
         if event.type == 3: #A stick is moved
             
             if event.code == 1: #Y axis on left stick
-                left_speed = scalestick(event.value)
+                left_speed = -scalestick(event.value)
                 if abs(left_speed) < 15: #deadzone
                     left_speed = 0
             '''
@@ -99,7 +99,7 @@ if __name__ == "__main__":
                     other_speed = 0
             '''
             if event.code == 5: #Y axis on right stick
-                right_speed = scalestick(event.value)
+                right_speed = -scalestick(event.value)
                 if abs(right_speed) < 15: #deadzone
                     right_speed = 0
 

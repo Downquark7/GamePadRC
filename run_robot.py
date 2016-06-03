87929d830f008ccc7dbaf2f06c7359967e15eb36
Downquark7 <mail2emn@gmail.com>
2016-06-02 20:37:52 -0500

    Update run_robot.py

--- run_robot.py
+++ run_robot.py
@@ -44,32 +44,32 @@ gamepad = evdev.InputDevice(ps3dev)
 
 
 
-left_speed = 0
-right_speed = 0
+forward_speed = 0
+turn_speed = 0
 #lift_speed = 0
 #other_speed = 0
 running = True
 
 class MotorThread(threading.Thread):
     def __init__(self):
-        #self.a_motor = ev3.LargeMotor(ev3.OUTPUT_A)
+        self.a_motor = ev3.LargeMotor(ev3.OUTPUT_A)
         self.b_motor = ev3.LargeMotor(ev3.OUTPUT_B)
         self.c_motor = ev3.LargeMotor(ev3.OUTPUT_C)
-        #self.d_motor = ev3.MediumMotor(ev3.OUTPUT_D)
+        self.d_motor = ev3.LargeMotor(ev3.OUTPUT_D)
         threading.Thread.__init__(self)
 
     def run(self):
         print "Engines running!"
         while running:
-            #self.a_motor.run_forever(duty_cycle_sp = dc_clamp(lift_speed))
-            self.b_motor.run_forever(duty_cycle_sp = dc_clamp(left_speed))
-            self.c_motor.run_forever(duty_cycle_sp = dc_clamp(right_speed))
-            #self.d_motor.run_forever(duty_cycle_sp = dc_clamp(other_speed))
+            self.a_motor.run_forever(duty_cycle_sp = dc_clamp(forward_speed + turn_speed))
+            self.b_motor.run_forever(duty_cycle_sp = dc_clamp(forward_speed + turn_speed))
+            self.c_motor.run_forever(duty_cycle_sp = dc_clamp(forward_speed - turn_speed))
+            self.d_motor.run_forever(duty_cycle_sp = dc_clamp(forward_speed - turn_speed))
 
-        #self.a_motor.stop()
+        self.a_motor.stop()
         self.b_motor.stop()
         self.c_motor.stop()
-        #self.d_motor.stop()
+        self.d_motor.stop()
 
 
 if __name__ == "__main__":
@@ -81,23 +81,23 @@ if __name__ == "__main__":
         if event.type == 3: #A stick is moved
             
             if event.code == 1: #Y axis on left stick
-                left_speed = -scalestick(event.value)
-                if abs(left_speed) < 15: #deadzone
-                    left_speed = 0
+                forward_speed = -scalestick(event.value)
+                if abs(forward_speed- < 15:
+                    forward_speed = 0
             '''
             if event.code == 0: #X axis on left stick
                 lift_speed = scalestick(event.value)
                 if abs(lift_speed) < 15: #deadzone
                     lift_speed = 0
+            '''
             if event.code == 2: #X axis on right stick
-                other_speed = scalestick(event.value)
-                if abs(other_speed) < 15: #deadzone
-                    other_speed = 0
+                turn_speed = scalestick(event.value)
+                if abs(turn_speed) < 15: #deadzone
+                    turn_speed = 0
             '''
             if event.code == 5: #Y axis on right stick
                 right_speed = -scalestick(event.value)
-                if abs(right_speed) < 15: #deadzone
-                    right_speed = 0
+            '''
 
         if event.type == 1 and event.code == 302 and event.value == 1:
             print "X button is pressed. Break."
